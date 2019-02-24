bbc93e05a65c74275107d861ea141a0e934e3af3
fedejimenez <fedej_mdp@hotmail.com>
2019-02-24 20:24:32 -0300

    change static route

--- trydjango/settings/production.py
+++ trydjango/settings/production.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_in_env")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_in_env")
 
 
 LOGGING = {
