1f20d749110b1cf5d5835d577d3687e16202b1c9
fedejimenez <fedej_mdp@hotmail.com>
2019-02-24 15:44:35 -0300

    Add whitenoise

--- trydjango/wsgi.py
+++ trydjango/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trydjango.settings')
 
 application = get_wsgi_application()
+
+
+# Using whitenoise
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
